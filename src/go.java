5e4e09d0ac62d9e42753adba4df31ce25ef1fef9
Nlake1285 <Mr.Skelly1285@gmail.com>
2024-10-27 11:10:03 -0600

    Set up Go game scoring, capturing, territory stuff, and a few game rules of Go

--- src/go.java
+++ src/go.java
@@ -3,35 +3,22 @@
 // By: Zenith Lake
 // Date: 10/28/2024
 //-------------------
+// Task:
+// Create a simple game of Go
 
-//Tasks:
-    // Create a program that will simulate the game of Go
-    // Create a two-dimentional array representation of a go Board
-    // Create a method that will print the goBoard
-    // Create a method that will allow the user to input their move
-    // Create a method that will allow the user to input their opponent's move or make the opponent move on their own
-    // TODO Create a method that will check if the move is valid
-    // TODO Create a method that will correctly detect if a stone is out of bounds
-    // TODO Create a method that will keep track of the score
-    // TODO Create a method that will keep track of the captured stones
-    // TODO Clean up the spaghetti code!!!!!!!!!!!!
-
-// Importing the Scanner class
 import java.util.Scanner;
-
-// Creating the Go class
 public class go {
-
-    // Creating the goBoard array!
-    static String[][] goBoard = new String[9][9];
-
-    // Main method
     public static void main(String[] args) throws Exception {
+        Scanner myObj = new Scanner(System.in); // Time to create the Scanner object
+        String[][] goBoard = new String[9][9]; // Creating the new go board array!
+        String[][] previousBoard = new String[9][9]; // Creating the previous board array!
+        int playerScore = 0; // Keeping track of the player's score
+        int opponentScore = 0; // Keeping track of the opponent's score
 
         while(true){
 
-            // Prints the board
-            System.out.println(" 1 2 3 4 5 6 7 8 9");
+            // Print the board
+            System.out.println(" 1 2 3 4 5 6 7 8 9"); // print the column numbers
             for (int i = 0; i < goBoard.length; i++){
                 System.out.print(i+1);
                 for (int j = 0; j < goBoard[i].length; j++){
@@ -50,130 +37,311 @@ public class go {
                 System.out.println();
             }
 
-            // Time to create the Scanner object
-            Scanner myObj = new Scanner(System.in);
+            // Store the current board state;
+            cloneBoard(goBoard, previousBoard);
+
+//------------------------------------------------------------------------------------------------
 
             // Player's move:
             System.out.println("Enter the x and y coordinates of your move: (Must be between 0 and 8) ");
             int moveX = myObj.nextInt();
             int moveY = myObj.nextInt();
-            goBoard[moveX][moveY] = "-o"; // Player's stone
 
             // Opponent's move:
             System.out.println("Enter the x and y coordinates of your opponent's move: (Must be between 0 and 8)");
             int opponentMoveX = myObj.nextInt();
             int opponentMoveY = myObj.nextInt();
-            goBoard[opponentMoveX][opponentMoveY] = "-*"; // Opponent's stone
 
             // Move verification:
-            if(goBoard[moveX][moveY] == "-o" || goBoard[opponentMoveX][opponentMoveY] == "-*"){
-                //TODO - Check if this works...
-                for (int i = 0; i < goBoard.length; i++){
-                    for (int j = 0; j < goBoard[i].length; j++){
-                        if(goBoard[i][j] == "-o" || goBoard[i][j] == "-*"){
-                            if(i < 0 || i > 8 || j < 0 || j > 8){
-                                System.out.println("Hey chucklenuts, your move is out of bounds!");
-                            }
-                        }
-                    }
+            boolean validMove = true;
+
+            if (moveX < 0 || moveX >= goBoard.length || moveY < 0 || moveY >= goBoard[0].length) {
+                System.out.println("Hey chucklenuts, your move is out of bounds!");
+                validMove = false;
+            } else if (opponentMoveX < 0 || opponentMoveX >= goBoard.length || opponentMoveY < 0 || opponentMoveY >= goBoard[0].length) {
+                System.out.println("Hey chucklenuts, your opponent's move is out of bounds!");
+                validMove = false;
+            }
+            if (validMove) {
+                // Proceed with placing the moves if nothing seems fishy...
+                if (goBoard[moveX][moveY] == null) {
+                    goBoard[moveX][moveY] = "-o";
+                } else {
+                    System.out.println("Hey chucklenuts, you can't place a stone there!");
+                }
+                if (goBoard[opponentMoveX][opponentMoveY] == null) {
+                    goBoard[opponentMoveX][opponentMoveY] = "-*";
+                } else {
+                    System.out.println("Hey chucklenuts, your opponent can't place a stone there!");
+                }
+
+                // Check for captures
+                playerScore += captureStones(goBoard, "-*");
+                opponentScore += captureStones(goBoard, "-o");
+
+                //Check for ko
+                if (isKo(goBoard, previousBoard)) {
+                    System.out.println("Ko rule violation! The move is not allowed.");
+                    // Undo the move:
+                    cloneBoard(previousBoard, goBoard);
                 }
             }
 
-            // insert new stuff here...
+            // Score calculation:
+            playerScore = calculateScore(goBoard, "-o");
+            opponentScore = calculateScore(goBoard, "-*");
+
+            // Score display:
+            System.out.println("Player Score: " + playerScore);
+            System.out.println("Opponent Score: " + opponentScore);
         }
     }
+
+//------------------------------------------------------------------------------------------------
+
+    // Method for score keeping:
+    public static int calculateScore(String[][] board, String playerStone) {
+        int score = 0;
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                if(board[i][j] == playerStone){
+                    score++;
+                }
+            }
+        }
+        return score;
+    }
+
+    // Method to calculate the territory
+    public static int calculateTerritory(String[][] board, String playerStone) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        int territory = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == null && !visited[i][j]) {
+                    int[] result = exploreTerritory(board, i, j, playerStone, visited);
+                    if (result[1] == 1) {
+                        territory += result[0];
+                    }
+                }
+            }
+        }
+        return territory;
+    }
+
+    // Method to explore a territory
+    public static int[] exploreTerritory(String[][] board, int x, int y, String playerStone, boolean[][] visited) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || visited[x][y]) {
+            return new int[]{0, 1};
+        }
+        visited[x][y] = true;
+        if (board[x][y] != null) {
+            return new int[]{0, board[x][y].equals(playerStone) ? 1 : -1};
+        }
+        int[] up = exploreTerritory(board, x - 1, y, playerStone, visited);
+        int[] down = exploreTerritory(board, x + 1, y, playerStone, visited);
+        int[] left = exploreTerritory(board, x, y - 1, playerStone, visited);
+        int[] right = exploreTerritory(board, x, y + 1, playerStone, visited);
+        int territory = 1 + up[0] + down[0] + left[0] + right[0];
+        int ownership = up[1] == down[1] && down[1] == left[1] && left[1] == right[1] ? up[1] : 0;
+        return new int[]{territory, ownership};
+    }
+
+    // Method to check if a group of stones has any liberties:
+    public static boolean hasLiberties(String[][] board, int x, int y, String stone, boolean[][] visited) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+            return false;
+        }
+        if (board[x][y] == null) {
+            return true;
+        }
+        if (visited[x][y] || !board[x][y].equals(stone)) {
+            return false;
+        }
+        visited[x][y] = true;
+        return  hasLiberties(board, x + 1, y, stone, visited) ||
+                hasLiberties(board, x - 1, y, stone, visited) ||
+                hasLiberties(board, x, y + 1, stone, visited) ||
+                hasLiberties(board, x, y - 1, stone, visited);
+    }
+
+    // Method for capturing stones:
+    public static int captureStones(String[][] board, String opponentStone) {
+        int capturedStones = 0;
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                if(board[i][j] == opponentStone){
+                    boolean[][] visited = new boolean[board.length][board[0].length];
+                    if (!hasLiberties(board, i, j, opponentStone, visited)) {
+                        board[i][j] = null;
+                        capturedStones++;
+                    }
+                }
+            }
+        }
+        return capturedStones;
+    }
+
+    // Method for removing stones from the board:
+    public static int removeStones(String[][] board, int x, int y, String stone) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] == null || !board[x][y].equals(stone)) {
+            return 0;
+        }
+        board[x][y] = null;
+        return 1  + removeStones(board, x + 1, y, stone) +
+                    removeStones(board, x - 1, y, stone) +
+                    removeStones(board, x, y + 1, stone) +
+                    removeStones(board, x, y - 1, stone);
+    }
+    
+    // Method to clone the board state
+    public static void cloneBoard(String[][] source, String[][] destination) {
+        for (int i = 0; i < source.length; i++) {
+            for (int j = 0; j < source[i].length; j++) {
+                destination[i][j] = source[i][j];
+            }
+        }
+    }
+
+//------------------------------------------------------------------------------------------------
+// Other Rules:
+
+    // Method to check if a point is an eye
+    public static boolean isEye(String[][] board, int x, int y, String playerStone) {
+        if (board[x][y] != null) {
+            return false;
+        }
+        boolean isEye = true;
+        if (x > 0 && !board[x - 1][y].equals(playerStone)) isEye = false;
+        if (x < board.length - 1 && !board[x + 1][y].equals(playerStone)) isEye = false;
+        if (y > 0 && !board[x][y - 1].equals(playerStone)) isEye = false;
+        if (y < board[0].length - 1 && !board[x][y + 1].equals(playerStone)) isEye = false;
+        return isEye;
+    }
+
+    // Method to check for ko
+    public static boolean isKo(String[][] currentBoard, String[][] previousBoard) {
+        for (int i = 0; i < currentBoard.length; i++) {
+            for (int j = 0; j < currentBoard[i].length; j++) {
+                if (currentBoard[i][j] != previousBoard[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Method to check for seki
+    public static boolean isSeki(String[][] board, String playerStone, String opponentStone) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != null && board[i][j].equals(playerStone)) {
+                    if (hasSharedLiberties(board, i, j, playerStone, opponentStone, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    // Method to check if a group has shared liberties with the opponent
+    public static boolean hasSharedLiberties(String[][] board, int x, int y, String playerStone, String opponentStone, boolean[][] visited) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+            return false;
+        }
+        if (visited[x][y]) {
+            return false;
+        }
+        visited[x][y] = true;
+        if (board[x][y] == null) {
+            return true;
+        }
+        if (board[x][y].equals(opponentStone)) {
+            return false;
+        }
+        return  hasSharedLiberties(board, x + 1, y, playerStone, opponentStone, visited) ||
+                hasSharedLiberties(board, x - 1, y, playerStone, opponentStone, visited) ||
+                hasSharedLiberties(board, x, y + 1, playerStone, opponentStone, visited) ||
+                hasSharedLiberties(board, x, y - 1, playerStone, opponentStone, visited);
+    }
 }
+//------------------------------------------------------------------------------------------------
+
 // Expected Output:
+/*
+ 1 2 3 4 5 6 7 8 9
+1|-|-|-|-|-|-|-|-|
+2|-|-|-|-|-|-|-|-|
+3|-|-|-|-|-|-|-|-|
+4|-|-|-|-|-|-|-|-|
+5|-|-|-|-|-|-|-|-|
+6|-|-|-|-|-|-|-|-|
+7|-|-|-|-|-|-|-|-|
+8|-|-|-|-|-|-|-|-|
+9|-|-|-|-|-|-|-|-|
+Enter the x and y coordinates of your move: (Must be between 0 and 8)
+1 1
+Enter the x and y coordinates of your opponent's move: (Must be between 0 and 8)
+0 0
+Player Score: 1
+Opponent Score: 1
+ 1 2 3 4 5 6 7 8 9
+1-*-|-|-|-|-|-|-|-|
+2|-o-|-|-|-|-|-|-|
+3|-|-|-|-|-|-|-|-|
+4|-|-|-|-|-|-|-|-|
+5|-|-|-|-|-|-|-|-|
+6|-|-|-|-|-|-|-|-|
+7|-|-|-|-|-|-|-|-|
+8|-|-|-|-|-|-|-|-|
+9|-|-|-|-|-|-|-|-|
+Enter the x and y coordinates of your move: (Must be between 0 and 8)
+8 8
+Enter the x and y coordinates of your opponent's move: (Must be between 0 and 8)
+0 1
+Player Score: 2
+Opponent Score: 2
+ 1 2 3 4 5 6 7 8 9
+1-*-*-|-|-|-|-|-|-|
+2|-o-|-|-|-|-|-|-|
+3|-|-|-|-|-|-|-|-|
+4|-|-|-|-|-|-|-|-|
+5|-|-|-|-|-|-|-|-|
+6|-|-|-|-|-|-|-|-|
+7|-|-|-|-|-|-|-|-|
+8|-|-|-|-|-|-|-|-|
+9|-|-|-|-|-|-|-|-o
 
-// Enter the coordinates of your move:
-// 1 1
-// Enter the coordinates of your opponent's move:
-// 2 2
-//  1 2 3 4 5 6 7 8 9
-// 1|-|-|-|-|-|-|-|-|
-// 2|-o-|-|-|-|-|-|-|
-// 3|-|-*-|-|-|-|-|-|
-// 4|-|-|-|-|-|-|-|-|
-// 5|-|-|-|-|-|-|-|-|
-// 6|-|-|-|-|-|-|-|-|
-// 7|-|-|-|-|-|-|-|-|
-// 8|-|-|-|-|-|-|-|-|
-// 9|-|-|-|-|-|-|-|-|
+... Some moves later...
 
-// Command line argument:
-// java Go.java
-
-
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Stuff that doesn't work yet:
-
-// // Creating a new go test board array!
-
-// static String[][] goTestBoard1 = {
-//     {null, null, "-*", "-*", null, null, null, null, null},
-//     {null, "-*", "-o", "-o", "-*", null, null, null, null},
-//     {null, "-*", "-o", null, "-o", "-*", null, null, null},
-//     {null, "-*", "-o", "-o", "-o", "-*", null, null, null},
-//     {null, "-*", "-o", null, "-o", "-*", null, null, null},
-//     {null, null, "-*", "-o", "-o", "-*", null, null, null},
-//     {null, null, null, "-*", "-*", null, null, null, null},
-//     {null, null, null, null, null, null, null, null, null},
-//     {null, null, null, null, null, null, null, null, null}
-//                                 };
-
-// // Creating other arrays for the game
-// static boolean[][] lives = new boolean[9][9];
-// static boolean[][] territory = new boolean[9][9];
-// static boolean[][] beenChecked = new boolean[9][9];
-
-// // Pseudocode for keeping the pieces alive
-// boolean[][] canBreathe(int x, int y) {
-            
-//     beenChecked[x][y] = true;
-
-//     if (goBoard[x][y] == null) {
-//         return true;}
-//     if (goBoard[x+1][y] == null) {
-//         return true;}
-//     if (goBoard[x-1][y] == null) {
-//         return true;}
-//     if (goBoard[x][y+1] == null) {
-//         return true;}
-//     if (goBoard[x][y-1] == null) {
-//         return true;}
-    
-//     return false;
-    
-// }
-
-
-// and...
-
-// // Detects if a stone is out of bounds
-//             // I don't think this works...
-//             //TODO - Fix this
-//             for (int i = 0; i < goBoard.length; i++){
-//                 for (int j = 0; j < goBoard[i].length; j++){
-//                     if(goBoard[i][j] == "-o" || goBoard[i][j] == "-*"){
-//                         if(i < 0 || i > 8 || j < 0 || j > 8){
-//                             System.out.println("This message appears when the out of bounds works!");
-//                         }
-//                     }
-//                 }
-//             }
-
-// and...
-
-// There are a few different levels to this problem of Capture though.
-            // You not only need to check if a piece just placed is alive,
-            // but you also need to check if in placing that piece you have captured the opposing pieces or if you have been captured yourself.
-            // This will come into play especially when working with the Go concept of "eyes" which are empty spaces left inside of solid blocks of stones.
-
-            // Method for score keeping:
-                //TODO - Create a method to keep track of the score
-                // ?????????????????????????????????????????????
-            
-            // Method to keep track of captured stones:
-                //TODO - Create a method to keep track of captured stones
-                // ?????????????????????????????????????????????
+Player Score: 7
+Opponent Score: 7
+ 1 2 3 4 5 6 7 8 9
+1-*-*-*-|-|-|-|-|-|
+2-*-o-|-|-|-|-|-|-|
+3-*-*-*-|-|-|-|-|-|
+4|-|-|-|-|-|-|-|-|
+5|-|-|-|-|-|-|-|-|
+6|-|-|-|-|-|-|-|-|
+7|-|-|-|-|-|-|-|-|
+8|-|-|-|-|-|-|-|-|
+9|-|-|-o-o-o-o-o-o
+Enter the x and y coordinates of your move: (Must be between 0 and 8)
+8 2
+Enter the x and y coordinates of your opponent's move: (Must be between 0 and 8)
+1 2
+Player Score: 7
+Opponent Score: 8
+ 1 2 3 4 5 6 7 8 9
+1-*-*-*-|-|-|-|-|-|
+2-*-|-*-|-|-|-|-|-|
+3-*-*-*-|-|-|-|-|-|
+4|-|-|-|-|-|-|-|-|
+5|-|-|-|-|-|-|-|-|
+6|-|-|-|-|-|-|-|-|
+7|-|-|-|-|-|-|-|-|
+8|-|-|-|-|-|-|-|-|
+9|-|-o-o-o-o-o-o-o
+*/
